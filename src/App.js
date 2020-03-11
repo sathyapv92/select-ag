a495540a174fe703f6760a08af9da13cfb09eb35
sathyapv92 <sathapv92@gmail.com>
2020-03-11 09:05:47 +0530

    t

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-responsive-modal';
 
+
 export default class App extends React.Component {
   state = {
     open: false,
@@ -19,6 +20,76 @@ export default class App extends React.Component {
     const { open } = this.state;
     return (
       <div>
+
+        <a className="button glow-button" target="_blank">Glow Button</a>
+        <div className="btn-group-toggle" data-toggle="buttons">
+
+          <input type="checkbox" checked autocomplete="off" />
+
+        </div>
+        <div>
+          <button href="#" className="btn btn-primary">Primary</button>
+
+
+          <button href="#" className="badge badge-primary">Primary</button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          <div class="container text-center">
+
+            <button type="button" className="btn btn-default">Default</button>
+
+
+            <button type="button" className="btn btn-primary">Primary</button>
+
+
+            <button type="button" className="btn btn-success">Success</button>
+
+
+            <button type="button" className="btn btn-info">Info</button>
+
+
+            <button type="button" className="btn btn-warning">Warning</button>
+
+
+            <button type="button" className="btn btn-danger">Danger</button>
+          </div>
+
+
+
+
+
+        </div>
         <button onClick={this.onOpenModal}>Open modal</button>
         <Modal open={open} onClose={this.onCloseModal} >
           <h2>Simple centered modal</h2>
